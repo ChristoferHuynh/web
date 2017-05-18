f4cf05edae748f8c9588ddc83657e0db65eca6d4
ChristoferHuynh <christofer.huynh@gmail.com>
2017-05-19 00:53:30 +0200

    fixes

--- web/run.sh
+++ web/run.sh
@@ -1,4 +1,5 @@
+DIR="$( cd "$( dirname "$0" )" && pwd )"
 export WEEKS_SECRET='something-really-secret'
-export FLASK_APP=/home/stoff/TrueSec/web/web/autoapp.py
+export FLASK_APP="$DIR/autoapp.py"
 export FLASK_DEBUG=1
 flask run
